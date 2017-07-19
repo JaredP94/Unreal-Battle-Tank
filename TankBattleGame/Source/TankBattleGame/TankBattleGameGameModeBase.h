3a44e1e1901aa003556c522551271ab2174401d4
JaredP94 <jaredping@yahoo.com>
2017-07-20 00:36:54 +0200

    Game Project Created

---
+++ TankBattleGame/Source/TankBattleGame/TankBattleGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankBattleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKBATTLEGAME_API ATankBattleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
