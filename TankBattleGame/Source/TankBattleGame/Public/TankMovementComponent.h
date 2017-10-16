d6257dbb4578674337fa3a0791e8d9ceda40f952
JaredP94 <jaredping@yahoo.com>
2017-10-16 07:48:04 +0200

    Tank Movement refactored

--- TankBattleGame/Source/TankBattleGame/Public/TankMovementComponent.h
+++ TankBattleGame/Source/TankBattleGame/Public/TankMovementComponent.h
@@ -26,9 +26,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendTurnRight(float Throw);
 
+private:
 	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 
-private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 	
