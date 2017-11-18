8678e0fe7bfa9f6eab445d345365fb670b877f3f
Jared <jaredping@yahoo.com>
2017-11-18 02:03:05 +0200

    Added first person camera view

--- TankBattleGame/Source/TankBattleGame/Private/TankPlayerController.cpp
+++ TankBattleGame/Source/TankBattleGame/Private/TankPlayerController.cpp
@@ -58,7 +58,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVec
 	FHitResult HitResult;
 	auto StartLocation = PlayerCameraManager->GetCameraLocation();
 	auto EndLocation = StartLocation + LookDirection * LineTraceRange;
-	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECollisionChannel::ECC_Visibility))
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECollisionChannel::ECC_Camera))
 	{
 		OutHitLocation = HitResult.Location;
 		return true;
